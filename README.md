80190a0a302c0ef9b25d2059b3370987282dab44
aaaaa <aaaa@gmail.com>
2019-06-11 13:24:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## 是什么？
+docker, Caddy, v2ray的一键式启动脚本，当前只支持 Ubuntu.
+
+
+## 怎么使用
+- 运行脚本  start-ubuntu.sh
+- 提示输入域名和邮箱，脚本会自动启动。
+- 启动完成后，拷贝客户端 client/config.json(Mac/Linux) 或者 client/config-win.json（Windows）
+    - Mac/Linux 需要创建日志目录: mkdir -p /var/log/v2ray/log/
+    - Windows 需要创建日志目录: D:\log
+
+    
---
+++ start-ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 修改v2ray的 uuid
+installed_uuid=`apt list |grep -w uuid | grep installed`
+if [ "$installed_uuid"x = "x" ] ; then
+        apt install -y uuid
+fi
+
+new_uuid=`uuid`
+sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' server/config.json
+sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' client/config-win.json
+sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' client/config.json
+
+# read domain
+
+read -p "domain for caddy : " domain
+read -p "email for tls : " mail
+
+sed -i -e 's/{DOMAIN}/'"$domain"'/g' Caddyfile
+sed -i -e 's/{DOMAIN}/'"$domain"'/g' client/config-win.json
+sed -i -e 's/{DOMAIN}/'"$domain"'/g' client/config.json
+
+sed -i -e 's/{MAIL}/'"$mail"'/g' Caddyfile
+
+docker-compose up -d
+
+echo "v2ray started an $domain with new UUID : $new_uuid"
+echo "copy client config file into local v2ray"
