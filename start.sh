e8c52dc36441ef4bc0f26793bfc857ca3a040e95
aaaaa <aaaa@gmail.com>
2019-08-08 21:47:02 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
-# generate new uuid
-new_uuid=$(cat /proc/sys/kernel/random/uuid)
+####### 参数解析 #######
+cmdname=$(basename $0)
+init=false
 
+# usage help doc.
+usage() {
+    cat << USAGE  >&2
+Usage:
+    $cmdname [-i]
+    -i         Init the run env. It will generate a uuid and replace config files with this uuid.
+    -h         Show help info.
+USAGE
+    exit 1
+}
 
-sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' v2ray-config.json
+while getopts ih OPT;do
+    case $OPT in
+        i)
+            init=true
+            ;;
+        h)
+            usage
+            exit 2
+            ;;
+        \?)
+            usage
+            exit 3
+            ;;
+    esac
+done
 
-# read domain and email from user input
-read -p "Domain for caddy : " domain
-read -p "Email for tls : " mail
+if [ "$init"x = "truex" ] ; then
+    # generate new uuid
+    new_uuid=$(cat /proc/sys/kernel/random/uuid)
 
-# replace domain and mail int Caddyfile
-sed -i -e 's/{DOMAIN}/'"$domain"'/g' Caddyfile
-sed -i -e 's/{MAIL}/'"$mail"'/g' Caddyfile
+    # replace v2ray server config
+    sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' v2ray-config.json
+
+    # read domain and email from user input
+    read -p "Domain for caddy : " domain
+    read -p "Email for tls : " mail
+
+    # replace domain and mail int Caddyfile
+    sed -i -e 's/{DOMAIN}/'"$domain"'/g' Caddyfile
+    sed -i -e 's/{MAIL}/'"$mail"'/g' Caddyfile
+fi
 
 # start caddy + v2ray
 docker-compose down
