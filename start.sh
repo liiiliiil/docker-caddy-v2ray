4c8580fe21f7ca6d82bf9c6375e777dce90f0281
aaaaa <aaaa@gmail.com>
2019-08-08 20:36:00 +0800

    delete client

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# generate new uuid
+new_uuid=$(cat /proc/sys/kernel/random/uuid)
+
+
+sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' v2ray-config.json
+
+# read domain and email from user input
+read -p "Domain for caddy : " domain
+read -p "Email for tls : " mail
+
+# replace domain and mail int Caddyfile
+sed -i -e 's/{DOMAIN}/'"$domain"'/g' Caddyfile
+sed -i -e 's/{MAIL}/'"$mail"'/g' Caddyfile
+
+# start caddy + v2ray
+docker-compose up -d
+
+# output uuid
+echo "v2ray started an $domain with new UUID : $new_uuid"
