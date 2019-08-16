b830dd990514dc20f17330d25836ad5a9212472e
aaaaa <aaaa@gmail.com>
2019-08-17 01:11:34 +0800

    add ss tcp

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 ## 是什么？
 docker, Caddy, v2ray的一键式启动脚本.
 
+当前配置的是 WS + TLS 的方式，所以需要域名。
 
 ## 怎么使用
-- 运行: 
+### 第一次运行
 
 ```
-git clone git@github.com:yuanmomo/v2ray-caddy-docker.git && cd v2ray-caddy-docker && start-xxxxx.sh
+git clone https://github.com/yuanmomo/docker-caddy-v2ray.git && cd v2ray-caddy-docker && start.sh -i 
 ```
-- 按提示输入域名和邮箱。
-- 启动完成后，拷贝客户端 client/config.json(Mac/Linux) 或者 client/config-win.json（Windows）
-    - Mac/Linux 需要创建日志目录: mkdir -p /var/log/v2ray/log/
-    - Windows 需要创建日志目录: D:\log
+然后按提示输入域名和邮箱。
 
-    
+如果使用了 CloudFlare ，注意在第一次启动的时候，因为需要去申请证书，验证，所以不要开启 CDN。否则 Caddy 会启动失败。
+
+如果启动有问题可以用下面两个命令查看日志。
+
+```shell
+docker logs caddy 
+docker logs v2ray 
+```
+
+### 重启
+
+```shell
+sh start.sh
+```
+执行脚本即可。
--- start.sh
+++ start.sh
@@ -33,10 +33,18 @@ done
 
 if [ "$init"x = "truex" ] ; then
     # generate new uuid
-    new_uuid=$(cat /proc/sys/kernel/random/uuid)
+    new_uuid_tcp=$(cat /proc/sys/kernel/random/uuid)
+    new_uuid_ws_tls=$(cat /proc/sys/kernel/random/uuid)
+    new_ss_pwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 24 | head -n 1)
+
+    # cp files
+    cp -fv Caddyfile.template Caddyfile
+    cp -fv v2ray-config.json.templatea v2ray-config.json
 
     # replace v2ray server config
-    sed -i -e 's/{UUID_PLACE_HOLDER}/'"$new_uuid"'/g' v2ray-config.json
+    sed -i -e 's/{UUID_PLACE_HOLDER_TCP}/'"$new_uuid_tcp"'/g' v2ray-config.json
+    sed -i -e 's/{UUID_PLACE_HOLDER_WS_TLS}/'"$new_uuid_ws_tls"'/g' v2ray-config.json
+    sed -i -e 's/{SS_PASSWORD}/'"$new_ss_pwd"'/g' v2ray-config.json
 
     # read domain and email from user input
     read -p "Domain for caddy : " domain
